4efdc037a7d012b5e24e61b7cc574306e1f78bb8
MGShahbazJahan <shahbazjahan9@gmail.com>
2022-10-30 11:16:39 +0530

    registration page changed

---
+++ email.js
@@ -0,0 +1,120 @@
+MongoClient = require("mongodb").MongoClient
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const path = require("path")
+const { devNull } = require("os")
+const { GridFSBucket } = require("mongodb")
+const { redirect } = require("express/lib/response")
+// const url = 'mongodb://localhost:27017/';
+const url =
+    "mongodb+srv://shahbazjahan9:shahbazjahan9@cluster0.vb8fvg4.mongodb.net/?retryWrites=true&w=majority"
+const databasename = "team6"
+const session = require("express-session")
+const { runInNewContext } = require("vm")
+const app = express()
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    })
+)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/email", function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+  
+      if (err) throw err;
+      var dbo = db.db(databasename);
+      dbo.collection("course").find().toArray(function (err, result) {
+        if (err) throw err;
+        //console.log(result);
+        d = {}
+        final = {}
+        users = {}
+        for (let i = 0; i < result.length; i++) {
+          d[result[i].course_id] = [];
+        }
+        dbo.collection("assignment_list").find().toArray(function (err, res) {
+          console.log(res);
+          for (let i = 0; i < res.length; i++) {
+            console.log(res[i].course_id);
+            key1 = res[i].assignment_id;
+            value = res[i].deadline
+            final[key1] = value;
+          }
+          //console.log("final: ",final);
+          dbo.collection("assignment_submission").find().toArray(function (err, output) {
+            console.log(output);
+  
+            if (err) throw err;
+            for (let i = 0; i < output.length; i++) {
+              users[output[i].assignment_id] = []
+              users[output[i].assignment_id].push(output[i].student_id);
+  
+  
+            }
+            console.log("users: ", users)
+            console.log(final)
+            for (let key in final) {
+              if (final.hasOwnProperty(key)) {
+                var value = final[key]
+                //  const va=value.split("T");
+                console.log("user enetered: ", value)
+                value.setDate(value.getDate() - 1);
+                var today = new Date()
+                console.log("User: ", value.getDate())
+                console.log(value.getDate() == today.getDate())
+                if (value.getDate() == today.getDate() && value.getMonth == today.getMonth() && value.getYear() == today.getYear()) {
+                  MongoClient.connect(url, function (err, db) {
+                    c
+                    if (err) throw err;
+                    //console.log("result:: ",result)
+                    var dbo = db.db(databasename);
+                    for (let i = 0; i < users[key].length; i++) {
+  
+                      query = { "username": users[key][i] }
+                      dbo.collection("login").find(query).toArray(function (err, ou) {
+                        if (err) throw err;
+                        console.log(ou)
+                        var mail = ou.email;
+                        console.log("Mail: ", mail);
+                        let mailDetails = {
+                          from: 'shahbazjahan5@gmail.com',
+                          to: mail,
+                          subject: 'ASSIGMENT SUBMISSION REMAINDER',
+                          text: "Submit the assignment "
+                        };
+  
+                        mailTransporter.sendMail(mailDetails, function (err, data) {
+                          if (err) {
+                            console.log('Error Occurs');
+                            console.log(err);
+                          } else {
+                            console.log('Email sent successfully');
+                          }
+                        });
+  
+                      });
+                    }
+                  });
+                }
+              }
+            }
+          });
+        });
+      });
+    });
+  });
+  
